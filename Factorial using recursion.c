5001afd2c977e4644f080fb17c3c34c6e7108c9f
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-01-14 10:24:06 +0530

    first commit

---
+++ Factorial using recursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int factorial(int n){
+	if(n == 0 || n == 1){
+		return 1;
+	}
+	else{
+		return (n * factorial(n-1));
+	}
+}
+
+void main(){
+	int num;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	printf("%d",factorial(num));
+	getch();
+}
